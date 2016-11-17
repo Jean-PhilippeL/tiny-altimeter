50f79159b0d172e73357e6edc731e0ccc50fda74
Jean-PhilippeL <jpletard@gmail.com>
2016-11-17 22:16:54 +0100

    Add files via upload
    Complete rewrite for button management using interrupts
    Add low power lib in order to improve uptime

--- tiny-altimeter.ino
+++ tiny-altimeter.ino
@@ -3,7 +3,7 @@
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <Button.h>
+//#include <Button.h>
 #include <EEPROM.h>
 #include "EEPROMAnything.h"
 #include <Dht11.h>
@@ -11,32 +11,13 @@
 #include <DS1307RTC.h>
 #include "LowPower.h"
 
-
-// If using software SPI (the default case):
-#define OLED_CS_PIN    0 //unused
-
-#define OLED_DC_PIN         6
-#define OLED_RESET_PIN      5
-#define OLED_MOSI_PIN       4
-#define OLED_CLK_PIN        3
-#define OLED_VCC_PIN        2
-//#define OLED_GND_PIN        4
-
-
-#define BUTTON_UP_PIN     10
-#define BUTTON_DOWN_PIN     9
+#define BUTTON_UP_PIN     3
+#define BUTTON_DOWN_PIN     2
 
 #define DHT11_PIN     8
-#define DHT11_VCC_PIN     7
 
-#define NO_SYMBOL -1
-#define SYMBOL_HPA 0
-#define SYMBOL_METER 1
-#define SYMBOL_DEG 2
-#define SYMBOL_MVOLT 3
-//#define SYMBOL_UP 4
-//#define SYMBOL_DOWN 5
-#define SYMBOL_PERCENT 6
+
+
 
 #define CURRENT_ALTITUDE_SCREEN 1
 #define MAX_ALTITUDE_SCREEN 2
@@ -47,12 +28,12 @@
 #define HUMIDITY_SCREEN 7
 //#define UPTIME_SCREEN 8
 #define HOUR_SCREEN 8
-#define MIN_SCREEN 9
-#define NB_SCREENS 9
+
+#define NB_SCREENS HOUR_SCREEN
 
 #define READ_DHT11_MAX_TRY 10
 
-//Adafruit_SSD1306 display(OLED_MOSI_PIN, OLED_CLK_PIN, OLED_DC_PIN, OLED_RESET_PIN, OLED_CS_PIN);
+
 
 Adafruit_SSD1306 display(-1);
 
@@ -70,12 +51,9 @@ Adafruit_SSD1306 display(-1);
 extern uint8_t Battery[];
 
 SFE_BMP180 pressure;
-Button buttonUp = Button(BUTTON_UP_PIN);
-Button buttonDown = Button(BUTTON_DOWN_PIN);
+//Button buttonUp = Button(BUTTON_UP_PIN);
+//Button buttonDown = Button(BUTTON_DOWN_PIN);
 
-
-
-int value = 0;
 boolean settingMode = false;
 double QNH, saveQNH;
 double temperature, pression, altitude = 0;
@@ -92,27 +70,13 @@ double samplesBuffer[MAX_SAMPLES];
 int indexBfr = 0;
 double averagePressure = 0;
 boolean bufferReady = false;
-int screen = CURRENT_ALTITUDE_SCREEN; // numero d'ecran
+int screen = HOUR_SCREEN; // numero d'ecran
 boolean skipClic = false;
 tmElements_t tm;
 
-unsigned long lastUpdate=0;
+volatile boolean buttonWasPressed = false;
 
 void setup()   {   
-  
-  //digitalWrite( OLED_VCC_PIN, HIGH);
-  //digitalWrite( DHT11_VCC_PIN, HIGH);
- // digitalWrite( OLED_GND_PIN, LOW);
-  
-  //digitalWrite( BUTTON_UP_PIN, HIGH); //active la pull up interne
-  //digitalWrite( BUTTON_DOWN_PIN, HIGH); //active la pull up interne
-  
-  buttonUp.clickHandler(handleButtonReleaseEvents);
-  buttonDown.clickHandler(handleButtonReleaseEvents);
-  buttonUp.holdHandler(handleButtonHoldEvents,1000);
-  buttonDown.holdHandler(handleButtonHoldEvents,1000);
-
- // display.begin(SSD1306_SWITCHCAPVCC);
   display.begin();
   display.setTextSize(1);
   display.setTextColor(WHITE);
@@ -128,51 +92,102 @@ void setup()   {
   }
   saveQNH = QNH;
 
-
   if (!pressure.begin()) {
-  
-  //display.setCursor(0,0);
     display.println(F("fail"));
     display.display();
     delay(3000); 
   }
 
+  pinMode(BUTTON_UP_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_DOWN_PIN, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(BUTTON_UP_PIN), test, FALLING );
+  attachInterrupt(digitalPinToInterrupt(BUTTON_DOWN_PIN), test2, FALLING );
+
 //TODO : diminuer luminosité :display.dim(true);
 }
 
-void loop() {
-  bool b1 = buttonUp.isPressed();
-  bool b2 = buttonDown.isPressed();
-  if(b1 && b2){
-    value = 0;
-    // inverse l'écran pour montrer que la manipulation à été prise en compte
-    display.invertDisplay(true);
-    delay(500);
-    display.invertDisplay(false);
-      if (screen == MAX_ALTITUDE_SCREEN || screen == MIN_ALTITUDE_SCREEN) {
-        resetAltiMinMax();
-      } else {
-        settingMode = !settingMode;
-      }   
-      }
-  
-  
-  
+void test(){
+  buttonWasPressed = true;
+}
 
-  if (settingMode) {
+void test2(){
+  buttonWasPressed = true;
+}
+
+void loop() {
+ 
+  if(buttonWasPressed){
+       manageButtons();
+      }
+  updatePressureAndTemperature();
+  refreshScreen();
+               
+   LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF); 
+}
+
+void refreshScreen(){
+   display.invertDisplay(settingMode);
+  /*if (settingMode) {
     displaySettings();
   } else {
-    if(millis() -1000 > lastUpdate){
-      updatePressureAndTemperature();
+    */  
        displayMainScreen();
-       lastUpdate = millis();
-    }
-               
-  }
-  //delay(50);
-  
-   LowPower.powerDown(SLEEP_60MS, ADC_OFF, BOD_OFF); 
+    //}
 }
+
+void manageButtons(){
+  unsigned long  milli = millis();
+  boolean upPressed = false;
+  boolean downPressed = false;
+
+  // debounce
+  while(millis() < milli + 400){
+    upPressed |=  !digitalRead(BUTTON_UP_PIN);
+    downPressed |= !digitalRead(BUTTON_DOWN_PIN);
+  }
+   
+  // 2 boutons en mm tps
+  if(upPressed &&  downPressed){
+        // inverse l'écran pour montrer que la manipulation à été prise en compte
+        display.invertDisplay(true);
+        //delay(500);
+        //display.invertDisplay(false);
+         //  if (screen == MAX_ALTITUDE_SCREEN || screen == MIN_ALTITUDE_SCREEN) {
+          //TODO   resetAltiMinMax();
+          //} else {
+            settingMode = !settingMode;
+          //} 
+  }
+  
+ 
+  // appui bref
+  if(upPressed && digitalRead(BUTTON_UP_PIN)){
+    handleButtonReleaseEvents(true, true);
+  }
+  if(downPressed && digitalRead(BUTTON_DOWN_PIN)){
+    handleButtonReleaseEvents(false, true);
+  }
+
+  // appui long
+   while(!digitalRead(BUTTON_UP_PIN)){
+    handleButtonReleaseEvents(true, false);
+    delay(1000);
+   } 
+  
+   while(!digitalRead(BUTTON_DOWN_PIN)){
+    handleButtonReleaseEvents(false, false);
+    delay(1000); //TODO a gérer avec millis
+   }
+
+    //TODO : rebond de release
+
+    //TODO : réactiver les interruptions
+
+   buttonWasPressed = false;
+}
+
+
 
 // Affiche les données d'un ecran
 void showScreen(String label, double value, String unit) {
@@ -187,9 +202,20 @@ void showScreen(String label, double value, String unit) {
   display.setCursor(0,10);
   display.setTextSize(4);
   display.print(buffer2);
-   display.setTextSize(1);
-   display.print(unit);
- 
+  display.setTextSize(1);
+  display.print(unit);
+
+
+
+//DEB
+
+    display.setCursor(0,55);
+    display.print("Alti : ");
+    display.println(baseAltitude, 0);
+
+//FIN
+
+  
   display.display();  
 }
 
@@ -210,10 +236,10 @@ void displayMainScreen(){
       EEPROM_writeAnything(eepromAddr, QNH);
     }
 
-     
-
+    
     switch (screen) {
     case CURRENT_ALTITUDE_SCREEN :
+
       if (lastValue != altitude) {
         showScreen(F("ALT."), altitude, "m");
         lastValue = altitude;
@@ -275,10 +301,7 @@ void displayMainScreen(){
       break;  */
 
    case HOUR_SCREEN:
-
-
- RTC.read(tm);
-
+  RTC.read(tm);
   display.clearDisplay(); 
   showBatterylevel(readVcc());
   display.setCursor(0,0);
@@ -290,22 +313,17 @@ void displayMainScreen(){
 
     sprintf(buffer2, "%2i", tm.Hour );
     display.print(buffer2);
-  display.setCursor(40,10);
+    display.setCursor(40,10);
     display.print(":");
-     display.setCursor(55,10);
- sprintf(buffer2, "%2i",tm.Minute );
-  display.print(buffer2);
-  display.setTextSize(1);
-  sprintf(buffer2, "%2i",tm.Second );
-  display.print(buffer2);
-  display.display();  
+    display.setCursor(55,10);
+    sprintf(buffer2, "%2i",tm.Minute );
+    display.print(buffer2);
+    display.setTextSize(1);
+    sprintf(buffer2, "%2i",tm.Second );
+    display.print(buffer2);
+    display.display();  
       break;  
-      
-   case  MIN_SCREEN:
-     RTC.read(tm);
-     showScreen("MIN", tm.Minute, "");
-      break;  
-      
+   
     }
 }
 
@@ -318,12 +336,7 @@ void displaySettings(){
     display.print("QNH : ");
     display.print(QNH,2);
     display.println(" hPa");    
-    //display.print("Debug : ");
-    //display.println(debugMsg);
-    //display.print("Etat : ");
-    //display.println(etat);
-    //if (etat == 1) drawSymbol(100, 40, SYMBOL_UP); //UP
-    //if (etat == 2) drawSymbol(100, 40, SYMBOL_DOWN); // DOWN
+    int value;
     if (value != 0) {
       baseAltitude += value; 
       value = 0; 
@@ -384,79 +397,60 @@ void setAltiMinMax() {
   if (altitude > altiMax) altiMax = altitude;
   if (altitude < altiMin) altiMin = altitude;
 }
+
+
 void resetAltiMinMax() {
   altiMax = altiMin = altitude;
 }
-// Gestion du bouton relaché
-void handleButtonReleaseEvents(Button &btn) {
-lastUpdate=0;
-if(skipClic){
-  skipClic = false;
-  return;
-}
-  
-      if (settingMode) { // Settings
-        if(btn == buttonUp){
-          value++;
-        } else {
-          value--;
-        }     
-      }
-      else { // Change screen
 
-        if(btn == buttonUp){
-            screen++;
-            if (screen > NB_SCREENS){
-              screen = 1;
-            } 
-        } else {
-          screen--;
-          if  (screen == 0){
+
+// Gestion du bouton relaché
+void handleButtonReleaseEvents(boolean up, boolean shortPush) {
+    if (settingMode) {
+           switch (screen) {
+              case CURRENT_ALTITUDE_SCREEN :
+                baseAltitude += (up?1:-1)*(shortPush?1:10);    
+                // recalcule le QNH
+                QNH = pressure.sealevel(pression, baseAltitude);
+                 updatePressureAndTemperature();
+                break;
+          
+              case MAX_ALTITUDE_SCREEN :
+                 altiMax =  altitude;
+                break;
+              case MIN_ALTITUDE_SCREEN :
+               altiMin =  altitude;
+                break;
+              case PRESSION_SCREEN :
+                break;
+              case TEMPERATURE_SCREEN : 
+                break;
+              case BATTERY_SCREEN :  
+                break;
+             case HUMIDITY_SCREEN :
+            
+                break;   
+             
+             case HOUR_SCREEN:
+                   RTC.read(tm); 
+                   tm.Hour+=(up?1:-1) * (shortPush?0:1);
+                   tm.Minute+=(up?1:-1) * (shortPush?1:0); 
+                   RTC.write(tm); 
+                   break;   
+              } 
+           
+    }
+    else { // Change screen
+      screen+=up?1:-1;
+      screen%=NB_SCREENS;
+      if  (screen == 0){
              screen = NB_SCREENS;
           } 
-        }
-
-        lastValue = 0;
-      }
-}
- 
-
-// Gestion de l'appui prolongé sur le bouton
-void handleButtonHoldEvents(Button &btn) {
-skipClic=true;
-
-   if (settingMode) { // Settings
-        if(btn == buttonUp){
-          value+=10;
-        } else {
-          value-=10;
-        }     
-      }
       
-    if(screen == HOUR_SCREEN){
-        RTC.read(tm); 
-         if(btn == buttonUp){
-            tm.Hour++;
-        } else {
-            tm.Hour--;
-        }   
-        RTC.write(tm); 
     }
-
-    if(screen == MIN_SCREEN){
-        RTC.read(tm); 
-         if(btn == buttonUp){
-            tm.Minute++;
-        } else {
-            tm.Minute--;
-        }   
-        RTC.write(tm); 
-    }
+    lastValue = 0; // ???
+     refreshScreen();
 }
-
-
-
-
 
 
 // Show battery level icon
